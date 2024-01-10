02824720b3fb3ca23abd4867501b8fe14752074d
ivan <ivanmw.150@gmail.com>
2024-01-09 23:31:32 -0600

    Mi perimera api

---
+++ src/main/java/com/example/apisalinas/ApiSalinasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisalinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSalinasApplication.class, args);
+	}
+
+}
